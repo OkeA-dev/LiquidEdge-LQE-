bbcff883c42c9f31267842753e3daaeecf7dede8
Oke.A <Okea.dev@gmail.com>
2024-07-15 21:34:31 +0100

    ...

--- src/LiquidEdge.sol
+++ src/LiquidEdge.sol
@@ -1,8 +1,34 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
-contract LiquidEdge is ERC20 {
+contract LiquidEdge is ERC20, Ownable, Pausable, ERC20Burnable {
 
+    constructor(uint256 initialSupply) ERC20("LiquidEdge", "LQE") Ownable(msg.sender) {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+ 
 }
+
+
+
